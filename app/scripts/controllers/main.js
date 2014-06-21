67a7e98cb8ccf0e78865f579a8266d64b09dde30
sen3ca <ilikegithub2>
2014-06-20 19:00:08 -0500

    support for dynamic iframe

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,11 +1,25 @@
 'use strict';
 
 angular.module('fiddleRipOffApp').controller('MainCtrl', function ($scope) {
-        $scope.html = '';
+        $scope.html = '<div class="red">hello there</div>';
         $scope.htmlRender = '';
-        $scope.css = '';
+        $scope.css = '.red { color:red }';
+        $scope.js = 'console.log("testing");';
         $scope.test = 'test';
         $scope.render = function () {
+
+            var inclusionScripts = '<script src="bower_components/jquery/jquery.js"><\/script>';
+            var script = '<script>' +
+                '$( document ).ready(function() {' +
+                    $scope.js +
+                '});' +
+                '<\/script>';
+            var html = $('#html').val();
+
+            document.getElementById('rendered').contentWindow.document.open();
+            document.getElementById('rendered').contentWindow.document.write('<html><head>' + inclusionScripts + '</head><body><style>' + $scope.css + '</style>' + $scope.html+script + '</body></html>');
+            document.getElementById('rendered').contentWindow.document.close();
+            //document.getElementById('rendered').contentDocument.location.reload(true);
             $scope.cssRender = $scope.css;
             $scope.htmlRender = $scope.html;
           };
